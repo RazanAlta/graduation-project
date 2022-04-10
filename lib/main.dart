d06115f6a58025db084ecbd0a6495a0d6fad6774
RazanAlta <65066845+RazanAlta@users.noreply.github.com>
2022-04-11 02:29:14 +0300

    Change

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:grad/MainPage/MainPage.dart';
+//import 'package:firebase_core/firebase_core.dart';
+//import 'firebase_options.dart';
 
 
 void main() {
